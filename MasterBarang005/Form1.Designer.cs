72d32b9cc015daf1d81783363d78f672a50d0168
Azhaar Fithri Shidqiyah <0320200005@polman.astra.ac.id>
2021-05-27 10:14:58 +0700

    Add project files.

---
+++ MasterBarang005/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace MasterBarang005
+{
+    partial class MasterBarang005
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbNamaSupp = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.txtHarga = new System.Windows.Forms.TextBox();
+            this.txtNamaBarang = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(76, 297);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(340, 150);
+            this.dataGridView1.TabIndex = 32;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(259, 245);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 31;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cbNamaSupp
+            // 
+            this.cbNamaSupp.FormattingEnabled = true;
+            this.cbNamaSupp.Items.AddRange(new object[] {
+            "Niken",
+            "Azhaar"});
+            this.cbNamaSupp.Location = new System.Drawing.Point(148, 203);
+            this.cbNamaSupp.Name = "cbNamaSupp";
+            this.cbNamaSupp.Size = new System.Drawing.Size(121, 21);
+            this.cbNamaSupp.TabIndex = 30;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Nama Supplier";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(148, 174);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 28;
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Location = new System.Drawing.Point(148, 148);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.Size = new System.Drawing.Size(100, 20);
+            this.txtHarga.TabIndex = 27;
+            // 
+            // txtNamaBarang
+            // 
+            this.txtNamaBarang.Location = new System.Drawing.Point(148, 122);
+            this.txtNamaBarang.Name = "txtNamaBarang";
+            this.txtNamaBarang.Size = new System.Drawing.Size(100, 20);
+            this.txtNamaBarang.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Stok";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Harga";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Nama Barang";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 15);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "MASTER BARANG";
+            // 
+            // MasterBarang005
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cbNamaSupp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.txtNamaBarang);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MasterBarang005";
+            this.Text = "MasterBarang005";
+            this.Load += new System.EventHandler(this.MasterBarang005_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbNamaSupp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.TextBox txtHarga;
+        private System.Windows.Forms.TextBox txtNamaBarang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MasterBarang005/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MasterBarang005
+{
+    public partial class MasterBarang005 : Form
+    {
+        SqlConnection con = new SqlConnection(@" data source = DESKTOP-2QPI586\SQLEXPRESS01; initial catalog = QuizMahasiswa;Integrated Security=True; ");
+        public MasterBarang005()
+        {
+            InitializeComponent();
+        }
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string nama_barang = txtNamaBarang.Text;
+            int harga = int.Parse(txtHarga.Text);
+            int stock = int.Parse(txtStock.Text);
+            string nama_supplier = cbNamaSupp.Text;
+            var data = new tbl_barang
+            {
+                nama_barang = nama_barang,
+                harga = harga,
+                stok = stock,
+                nama_supplier = nama_supplier,
+            };
+            db.tbl_barangs.InsertOnSubmit(data);
+            db.SubmitChanges();
+            MessageBox.Show("Save Successfully");
+            txtNamaBarang.Clear();
+            txtHarga.Clear();
+            txtStock.Clear();
+            cbNamaSupp.Items.Clear();
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            var st = from tb in db.tbl_barangs select tb;
+            dataGridView1.DataSource = st;
+        }
+
+        private void MasterBarang005_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select isnull(max (cast (ID as int)),0) +1 from TB_tbl_barang", con);
+            DataTable dt = new DataTable();
+
+            LoadData();
+        }
+    }
+}
